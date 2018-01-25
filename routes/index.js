1cd9348d01077e2b49cc4a071635139c514ca757
BolajiOlajide <bolaji.olajide@andela.com>
2018-01-26 00:28:01 +0100

    📖 templating with .pug

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,19 @@ const router = express.Router();
 
 // Do work here
 router.get('/', (req, res) => {
-  res.send('Hey! It works!');
+  // res.send('Hey! It works!');
+  res.render('hello', {
+    name: 'Bolaji',
+    dog: 'Vue'
+  });
+});
+
+
+router.get('/reverse/:name', (req, res) => {
+  // const { name } = req.params;
+  // res.send('it works!');
+  const reverse = [...req.params.name].reverse().join('');
+  res.json(reverse);
 });
 
 module.exports = router;
