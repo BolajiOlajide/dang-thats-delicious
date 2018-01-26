fcf26095d415a1c46027d9c66492e3ca2633b356
BolajiOlajide <bolaji.olajide@andela.com>
2018-01-26 01:06:50 +0100

    middlewates & error handling

--- controllers/home.controller.js
+++ controllers/home.controller.js
@@ -1,8 +1,14 @@
 exports.homePage = (req, res) => {
   // res.send('Hey! It works!');
+  console.log(req.name);
   res.render('hello', {
     name: 'Bolaji',
     dog: 'Vue',
     title: 'Welcome'
   });
 };
+
+exports.myMiddleware = (req, res, next) => {
+  req.name = 'Bolaji';
+  next()
+}
